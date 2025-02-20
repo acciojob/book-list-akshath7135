27e67cafc05806ff2a41120b9305f44b89fb0866
akshath7135 <akshathabrol21122004@gmail.com>
2025-02-20 12:05:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.getElementsById("submit").addEventListener("click",addBook);
+
+function addBook(event) {
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbnn = document.getElementById("isbn").value;
+
+	let rw = document.createElement("tr");
+	rw.innerHTML=`<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbnn}</td>
+	<td><button class="delete">X</button></td>`;
+
+	document.getElementById("book-list").appendChild(rw);
+
+	row.querySelector(".delete").addEventListener("click", () => {
+        row.remove();
+    });
+}
